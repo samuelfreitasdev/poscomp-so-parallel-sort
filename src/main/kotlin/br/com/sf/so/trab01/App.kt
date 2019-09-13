1376c7a72662dd0a753b28334c345e177785f0d9
Samuel Freitas <fresamuel@gmail.com>
2019-09-13 03:14:21 -0300

    MergeSort version 1.

--- src/main/kotlin/br/com/sf/so/trab01/App.kt
+++ src/main/kotlin/br/com/sf/so/trab01/App.kt
@@ -9,7 +9,7 @@ import java.time.Instant
 import java.util.*
 import java.util.concurrent.Semaphore
 
-class App {
+public class App {
 
     private val semaphore = Semaphore(1)
     private val list = LinkedList()
@@ -32,10 +32,7 @@ class App {
         println("The sorting takes $timeElapsed Milliseconds.")
 
         println("Writing sorted numbers in a file")
-        DataWriter(
-            "result-with-$availableThreads-threads.txt",
-            list
-        ).write()
+        DataWriter("output.tx", list).write()
 
         println("================================")
         println()
@@ -53,31 +50,33 @@ class App {
 
     companion object {
         const val FILES_NUMBER = 10
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Starting...")
+
+//            println("Should generate files(S or N)? ")
+//            if (readLine().equals("S", true)) {
+//                print("Type the entries amount per file: ")
+//                val amount = readLine()
+//
+//                AppDataGenerator().generate(amount!!.toInt())
+//            }
+
+            val reportData = ArrayList<Pair<Int, Long>>()
+            for (availableThreads in 0..20) {
+                reportData.add(Pair(availableThreads, App().run(availableThreads)))
+            }
+
+            println("REPORT\n")
+            println(String.format("%10s | %s ", "Threads", "Duration"))
+            for (data in reportData) {
+                println(String.format("%10d | %5d ms ", data.first, data.second))
+            }
+        }
+
     }
 }
 
 //-Xmx1G -Xms1G -Xss1G
-fun main() {
-
-    println("Starting...")
-
-//    println("Should generate files(S or N)? ")
-//    if (readLine().equals("S", true)) {
-//        print("Type the entries amount per file: ")
-//        val amount = readLine()
-//
-//        AppDataGenerator().generate(amount!!.toInt())
-//    }
-
-    val reportData = ArrayList<Pair<Int, Long>>()
-    for (availableThreads in 0..13) {
-        reportData.add(Pair(availableThreads, App().run(availableThreads)))
-    }
-
-    println("REPORT\n")
-    println(String.format("%10s | %s ", "Threads", "Duration"))
-    for (data in reportData) {
-        println(String.format("%10d | %5d ms ", data.first, data.second))
-    }
-
-}
+//-Xss10M
--- src/main/kotlin/br/com/sf/so/trab01/collection/sort/ParallelMergeSort.kt
+++ src/main/kotlin/br/com/sf/so/trab01/collection/sort/ParallelMergeSort.kt
@@ -1,18 +1,17 @@
 package br.com.sf.so.trab01.collection.sort
 
 import br.com.sf.so.trab01.collection.LinkedList
+import java.util.concurrent.RecursiveTask
 
-class ParallelMergeSort : MergeSort() {
+class ParallelMergeSort(
+    private val node: LinkedList.Node?,
+    private val availableThreads: Int
+) : RecursiveTask<LinkedList.Node?>() {
 
-    private fun mergeSort(
-        node: LinkedList.Node?,
-        availableThreads: Int
-    ): LinkedList.Node? {
-
+    override fun compute(): LinkedList.Node? {
         if (availableThreads <= 1) {
-            return mergeSort(node)
+            return sort(node)
         } else {
-
             if (node?.next == null) {
                 return node
             }
@@ -24,41 +23,81 @@ class ParallelMergeSort : MergeSort() {
             // set the next of middle node to null
             middle.next = null
 
-            var left: LinkedList.Node? = null
-            var right: LinkedList.Node? = null
+            val left = ParallelMergeSort(node, availableThreads - 1)
+            val right = ParallelMergeSort(nextOfMiddle, availableThreads - 1)
 
-            // Apply mergeSort on left list
-            val leftThread = object : Thread() {
-                override fun run() {
-                    left = mergeSort(node, availableThreads - 1)
-                }
-            }
+            left.fork()
 
-            // Apply mergeSort on right list
-            val rightThread = object : Thread() {
-                override fun run() {
-                    right = mergeSort(nextOfMiddle, availableThreads - 1)
-                }
-            }
-
-            leftThread.start()
-            rightThread.start()
-
-            try {
-                leftThread.join()
-                rightThread.join()
-            } catch (uncaughtExceptionHandler: InterruptedException) {
-                uncaughtExceptionHandler.printStackTrace()
-            }
-
-            // Merge the left and right lists
-            return sortedMerge(left, right)
+            return merge(left.join(), right.compute())
         }
+    }
+
+    private fun sort(head: LinkedList.Node?): LinkedList.Node? {
+        // Base case : if head is null
+        if (head?.next == null) {
+            return head
+        }
+
+        // get the middle of the list
+        val middle = getMiddle(head)
+        val nextOfMiddle = middle!!.next
+
+        // set the next of middle node to null
+        middle.next = null
+
+        // Apply mergeSort on left list
+        val left = sort(head)
+
+        // Apply mergeSort on right list
+        val right = sort(nextOfMiddle)
+
+        // Merge the left and right lists
+        return merge(left, right)
+    }
+
+    private fun merge(left: LinkedList.Node?, right: LinkedList.Node?): LinkedList.Node? {
+        val result: LinkedList.Node?
+        /* Base cases */
+        if (left == null)
+            return right
+        if (right == null)
+            return left
+
+        /* Pick either left or right, and recur */
+        if (left.value <= right.value) {
+            result = left
+            result.next = merge(left.next, right)
+        } else {
+            result = right
+            result.next = merge(left, right.next)
+        }
+        return result
+    }
+
+    // Utility function to get the middle of the linked list
+    private fun getMiddle(h: LinkedList.Node?): LinkedList.Node? {
+        // Base case
+        if (h == null)
+            return h
+
+        var slow = h
+        var fast = h.next
+
+        // Move fastptr by two and slow ptr by one
+        // Finally slowptr will point to middle node
+        while (fast != null) {
+            fast = fast.next
+            if (fast != null) {
+                slow = slow!!.next
+                fast = fast.next
+            }
+        }
+        return slow
     }
 
     companion object {
         fun sort(list: LinkedList, availableThreads: Int) {
-            list.head = ParallelMergeSort().mergeSort(list.head, availableThreads)
+            list.head = ParallelMergeSort(list.head, availableThreads).compute()
         }
     }
 
